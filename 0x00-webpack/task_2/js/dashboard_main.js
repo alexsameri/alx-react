0d6fea13971f5c6321fe3443a1ee048c80dd94ed
alexsameri <samerialexx@gmail.com>
2025-04-28 07:11:45 +0300

    loading css

---
+++ 0x00-webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from "jquery";
+import _ from "lodash";
+import 'style-loader!css-loader!../css/main.css';
+import logoImage from '../assets/holberton-logo.png';
+
+$(document).ready(function() {
+    $('body').append('<div id="logo"></div>');
+    $('body').append('<p>ALX Dashboard</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
+    const button = $('<button>Click here to get started</button>');
+    $('body').append(button);
+    $('body').append('<p id="count"></p>');
+    $('body').append('<p>Copyroght - ALX</P>');
+
+    let count = 0;
+    function updateCounter() {
+        count += 1;
+        $('#count').text(`${count} clicks on the button`);
+    }
+
+    const debouncedUpdateCounter = _.debounce(updateCounter, 500); // 500ms delay to prevent spamming
+    button.on('click', debouncedUpdateCounter);
+});
